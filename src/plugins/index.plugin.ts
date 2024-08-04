15c9ddcbe01bef8727e203bdf2c94a2929722074
Iasz7 <szapiroilan@gmail.com>
2024-08-04 12:14:33 -0300

    ADD: winston logger created

---
+++ src/plugins/index.plugin.ts
@@ -0,0 +1,6 @@
+import buildLogger from './logger.plugin';
+
+export {
+    buildLogger,
+
+};
---
+++ src/plugins/logger.plugin.ts
@@ -0,0 +1,37 @@
+import winston from 'winston';
+const {json, combine, timestamp, align, printf} =winston.format;
+
+const logger = winston.createLogger({
+    level: process.env.LOG_LEVEL || 'info',
+      format: combine(
+    // colorize({ all: true }),
+    timestamp({
+      format: 'YYYY-MM-DD hh:mm:ss.SSS A',
+    }),
+    align(),
+    printf((info) => `[${info.timestamp}] ${info.level}: ${info.message}`),
+    // json()
+  ),
+//   defaultMeta: { service: 'user-service' },
+  transports: [
+    //
+    // - Write all logs with importance level of `error` or less to `error.log`
+    // - Write all logs with importance level of `info` or less to `combined.log`
+    //
+    new winston.transports.File({ filename: 'logs/error.log', level: 'error' }),
+    new winston.transports.File({ filename: 'logs/combined.log' }),
+  ],
+});
+
+if (process.env.NODE_ENV !== 'production') {
+    logger.add(new winston.transports.Console({
+      format: winston.format.simple(),
+    }));
+  }
+
+export default function buildLogger(service: string) {
+    return {
+        log:(message: string) => logger.log('info', {message, service}),
+        error:(message: string) => logger.log('error', {message, service})
+    }
+};
